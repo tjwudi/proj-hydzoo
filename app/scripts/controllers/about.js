4c98f0f9fc6658250a4207aa74f8064d4dd8c166
tjwudi <webmaster@leapoahead.com>
2016-06-16 16:21:16 +0800

    Initial Commit
    Signed-off-by: tjwudi <webmaster@leapoahead.com>

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hydzooApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hydzooApp
+ */
+angular.module('hydzooApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hydzooApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hydzooApp
+ */
+angular.module('hydzooApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
